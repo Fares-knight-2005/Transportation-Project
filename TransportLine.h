c2815eeebe2fa25438380f42fd58dd032541dee5
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-09 23:14:20 +0300

    Update TransportLine.h

--- TransportLine.h
+++ TransportLine.h
@@ -13,7 +13,6 @@ class TransportLine
     private:
         DoubleLinkedList<Parking> parkings;
         int id,numberOfVehicles;
-        double totalLength;
         static int numberOfAllTransportLine;
 
 };
