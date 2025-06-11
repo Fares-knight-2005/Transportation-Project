a0f5ffe0e027a82d60e3afeeb06e4bc2f4b46c70
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-11 21:59:31 +0300

    Update clsVehicle.h

--- clsVehicle.h
+++ clsVehicle.h
@@ -4,4 +4,40 @@
 
 using namespace std;
 
+        enum enVehicleType {BUS=1, TRAM, FERRY, METRO};
 
+       string getVehicleType(enVehicleType vehicleType) {
+       switch(vehicleType) {
+        case BUS:  return "BUS";
+        case TRAM: return "TRAM";
+        case FERRY: return "FERRY";
+        default: return "METRO";
+       }
+       }
+
+       enVehicleType setVehicleType(int vehicleNumber){
+       switch(vehicleNumber){
+       case 1:return BUS;
+       case 2:return TRAM;
+       case 3:return FERRY;
+       default:return METRO;
+       }
+       }
+
+       class clsVehicle{
+       public:
+
+
+
+
+
+
+       private:
+       enVehicleType vehicleType;
+       
+
+
+
+        };
+
+      
