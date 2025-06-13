dcc959cc080dd8be5e347cc3c04c4e6e63ea34ac
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-13 09:58:23 +0300

    Update Database.h

--- Database.h
+++ Database.h
@@ -48,6 +48,57 @@ class Database
 			return Results.substr(Delim.length(), Results.length() - Delim.length());
 		}
 
+               void saveTransportLines(string filename, OpenHash<int, clsTransportLine>& transportLines) {
+               ofstream outFile(filename);
+    
+               for (int i = 0; i < transportLines.capacity; i++) {
+                 HashNode<clsTransportLine> current = transportLines.array[i].getHead();
+                 while (current != nullptr) {
+                      outFile << current->data.item.toString() << endl;
+                      current = current->next;
+                }
+                }
+    
+                outFile.close();
+                }
+
+                OpenHash<int, clsTransportLine> loadTransportLines(const string& filename) {
+    
+                OpenHash<int, clsTransportLine> transportLines;
+	        OpenHash<int,clsStation> stations;//يجب تحميله من تابع staions
+		    
+                ifstream file(filename);
+                string line;
+
+                while (getline(file, line)) {
+                  try {
+                   DoubleLinkedList<string>  tokens = Split(line, ",,,");
+                    if (tokens.size() < 4) 
+			    continue;
+              int id = stoi(tokens[0]);
+              int vehicles = stoi(tokens[1]);
+              double price = stod(tokens[2]);
+              enVehicleType type = static_cast<enVehicleType>(stoi(tokens[3]));
+              DoubleLinkedList<clsStation> s;
+              for (int i = 4; i < tokens.size(); i++) {
+                  int stationId = stoi(tokens[i]);
+                  s.addFirst(stations[stationId]);
+                }
+              }
+
+            clsTransportLine tl(vehicles, price, type, s);
+            tl.setid(id);
+            transportLines.insert(id, tl);
+
+           } catch (...) {
+              continue; 
+           }
+           }
+
+            file.close();
+            return transportLines;
+           }
+
 };
 
 #endif // DATABASE_H
