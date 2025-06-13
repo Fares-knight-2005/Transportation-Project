304bd3d65efe8867061b9e2610af227e23c61fa4
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-13 13:34:44 +0300

    Create clsVehicleTrip.h

---
+++ clsVehicleTrip.h
@@ -0,0 +1,20 @@
+#include<iostream>
+
+class clsVehicleTrip{
+
+struct VehicleMovements{
+int idStation;
+int currDisabilit,currPackageSize;
+SingleLinkedList<passengerTrip>> p;
+};
+
+int id,idVehicle;
+ClosedHash<int,VehicleMovements> m;
+public:
+
+int getId(){
+      return id;
+}
+
+
+};
