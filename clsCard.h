9f16131c66da33e4bf7a86ff2d043b5301fac5c9
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-13 11:52:42 +0300

    Update clsCard.h

--- clsCard.h
+++ clsCard.h
@@ -6,22 +6,30 @@ using namespace std;
 class clsCard {
 
 protected:
-	string type;
+	bool type;
 	double balance;
 
 
 public:
-	clsCard(string type = "Reguler", int balance = 0) {
+	clsCard(bool type = false, int balance = 0) {
 		this->type = type;
 		this->balance = balance;
 	}
-	virtual void payment(double amount) {
+
+        clsCard(string type = "Reguler", int balance = 0) {
+		if(type=="Reguler")
+			this.type=false;
+		else
+			this.type=true;
+		this->balance = balance;
+	}
+
+	void payment(double amount) {
 		if (balance >= amount) {
 			balance -= amount;
 		}
-		else {
-		}
 	}
+
 	void recharge(double amount) {
 		balance += amount;
 	}
@@ -30,13 +38,16 @@ public:
 		return balance;
 	}
 
-	string getType()  {
-		return type;
-	}
 
+        string getType(){
+        return type ? "Premium" : "Regular";
+        }
 
-
-
+       string toString(){
+        ostringstream oss;
+        oss << (type ? "Premium" : "Regular") << ",,," << balance;
+        return oss.str();
+       }
 
 
 
