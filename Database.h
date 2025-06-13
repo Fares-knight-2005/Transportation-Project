49a19dcafc04d4b00e87873a3d61dd004483203c
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-13 16:11:56 +0300

    Update Database.h

--- Database.h
+++ Database.h
@@ -259,4 +259,73 @@ static OpenHash<int, clsVehicle> loadVehicles(string filename) {
         return passengers;
     }
 
+static void saveVehicleTrips(const string& filename, OpenHash<int, clsVehicleTrip>& trips) {
+    ofstream outFile(filename);
+    
+    for (int i = 0; i < trips.capacity; i++) {
+        HashNode<int, clsVehicleTrip> current = trips.getHead(i);
+        while (current != nullptr) {
+            outFile << current->data.item.toString() << endl;
+            current = current->next;
+        }
+    }
+    
+    outFile.close();
+}
+
+static OpenHash<int, clsVehicleTrip> loadVehicleTrips(string filename) {
+    OpenHash<int, clsVehicleTrip> trips;
+    ifstream file(filename);
+    string line;
+
+    while (getline(file, line)) {
+        try {
+            DoubleLinkedList<string> tokens = Split(line, ",,,");
+            if (tokens.size() < 2) 
+                continue;
+
+            int id = stoi(tokens[0]);
+           
+            
+            clsVehicleTrip trip(id);
+            
+            int tokenIndex = 1;
+            while (tokenIndex < tokens.size()) {
+                int stationId = stoi(tokens[tokenIndex++]);
+                
+                strVehicleMovements movement(stationId);
+               
+                while (tokenIndex < tokens.size()) {
+                    try {
+                        int start = stoi(tokens[tokenIndex++]);
+                        int end = stoi(tokens[tokenIndex++]);
+                        int pid = stoi(tokens[tokenIndex++]);
+                        bool heading = tokens[tokenIndex++] == "1";
+                        bool disabled = tokens[tokenIndex++] == "1";
+                        bool items = tokens[tokenIndex++] == "1";
+                        
+                        clsPassengerTrip passenger(start, end, pid, heading, disabled, items);
+                        movement.passenger.addLast(passenger);
+                    } catch (...) {
+                        break;
+                    }
+                }
+                
+                trip.vehicleMovements.insert(stationId, movement);
+            }
+            
+            trips.insert(id, trip);
+            
+            if(clsVehicleTrip::numberOfAllVehicleTrip < id)
+                clsVehicleTrip::numberOfAllVehicleTrip = id;
+                
+        } catch (...) {
+            continue;
+        }
+    }
+
+    file.close();
+    return trips;
+   }
+
 #endif // DATABASE_H
