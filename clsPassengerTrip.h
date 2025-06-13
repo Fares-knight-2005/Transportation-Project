46668288e2a0d52f00ba264fa7d749445bac01be
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-13 08:07:08 +0300

    Update clsPassengerTrip.h

--- clsPassengerTrip.h
+++ clsPassengerTrip.h
@@ -3,9 +3,14 @@ using namespace std;
 
 class clsPassengerTrip{
 int idStarStation,idEndStation,idPassenger;
+bool destination;
+
 public:
     clsPassengerTrip(int idStarStation,int idEndStation,int idPassenger):idPassenger(idPassenger),idEndStation(idEndStation),idPassenger(idPassenger) {}
     int getIdEndStation(){
         return idEndStation; 
+    }
+    bool getDestination(){
+        return destination;    
     }
 };
