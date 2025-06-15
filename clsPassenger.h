770de0f8f380e81866d10f16a07473432671eb44
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-15 14:06:12 +0300

    Update clsPassenger.h

--- clsPassenger.h
+++ clsPassenger.h
@@ -12,27 +12,24 @@ class clsPassenger : public clsPerson {
 protected:
     clsCard Card;
     string Password;
+    bool isDisabled;
     static int numberOfAllPassenger;
     
 public:
     clsPassenger(short Age, string FirstName, string LastName, string PhoneNumber, string Email,
-                 int id, clsCard card, string password = "")
+                 int id, clsCard card, string password ,bool isDisabled)
         : clsPerson(id, Age, FirstName, LastName, PhoneNumber, Email), 
-          Card(card), Password(password) {}
+          Card(card), Password(password),isDisabled(isDisabled) {}
 
     clsPassenger(short Age, string FirstName, string LastName, string PhoneNumber, string Email,
-                 clsCard card, string password = "")
+                 clsCard card, string password , bool isDisabled)
         : clsPerson(Age, FirstName, LastName, PhoneNumber, Email), 
-          Card(card), Password(password) {
+          Card(card), Password(password),isDisabled(isDisabled) {
             this->Id=++numberOfAllPassenger;
           }
 
     string getPassword() {
         return Password;
-    }
-
-    int getId(){
-       return id;
     }
 
     void setPassword(string& newPassword) {
@@ -41,13 +38,13 @@ public:
 
     string toString() {
         ostringstream oss;
-        oss << personToString() << ",,," << Card.toString() << ",,," << Password;
+        oss << personToString() << ",,," << Card.toString() << ",,," << Password<< ",,," <<isDisabled;
         return oss.str();
     }
 
     static clsPassenger parse(string line) {
         DoubleLinkedList<string> tokens = Split(line, ",,,");
-        if (tokens.size() < 9) {
+        if (tokens.size() < 10) {
             throw invalid_argument("Not enough tokens in line");
         }
 
@@ -61,11 +58,12 @@ public:
         bool cardType = (tokens[6] == "Premium");
         double cardBalance = stod(tokens[7]);
         string password = tokens[8];
+        bool isDisabled= (stoi(tokens[9])==1);
         
         clsCard card(cardType, cardBalance);
         
-        return clsPassenger(age, firstName, lastName, phoneNumber, email, id, card, password);
+        return clsPassenger(age, firstName, lastName, phoneNumber, email, id, card, password, isDisabled);
     }
 };
 
-static int clsPassenger::numberOfAllPassenger = 0;
+int clsPassenger::numberOfAllPassenger = 0;
