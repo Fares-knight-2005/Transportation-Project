9efb2d108acb62de3611ac9b0e4d1417cfb45526
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-15 13:36:13 +0300

    Update clsTransportLine.h

--- clsTransportLine.h
+++ clsTransportLine.h
@@ -8,7 +8,7 @@
 class clsTransportLine
 {
 private:
-    DoubleLinkedList<clsStation*> stations;
+    DoubleLinkedList<clsStation> stations;
     string name;
     int id, numberOfVehicles;
     double price;
@@ -16,19 +16,19 @@ private:
     static int numberOfAllTransportLines;
 
 public:
-    clsTransportLine(int numberOfVehicles, double price, enVehicleType vehicleType, string name, DoubleLinkedList<clsStation*> stations) :
+    clsTransportLine(int numberOfVehicles, double price, enVehicleType vehicleType, string name, DoubleLinkedList<clsStation> stations) :
         stations(stations), price(price), id(++numberOfAllTransportLines), vehicleType(vehicleType),
         numberOfVehicles(numberOfVehicles), name(name) {
     }
 
-    clsTransportLine(int id, int numberOfVehicles, double price, enVehicleType vehicleType, string name, DoubleLinkedList<clsStation*> stations) :
+    clsTransportLine(int id, int numberOfVehicles, double price, enVehicleType vehicleType, string name, DoubleLinkedList<clsStation> stations) :
          id(id), stations(stations), price(price), vehicleType(vehicleType),
          numberOfVehicles(numberOfVehicles), name(name) {
     }
 
     clsTransportLine() : id(0), numberOfVehicles(0), price(0) {}
 
-    void addStation(clsStation* station, int stationNumber) {
+    void addStation(clsStation station, int stationNumber) {
         stations.add(stationNumber - 1, station);
     }
 
@@ -39,7 +39,7 @@ public:
             cout << "The station ID number you entered is not available.\n";
     }
 
-    DoubleNode<clsStation*>* getFirstStation(){
+    DoubleNode<clsStation>* getFirstStation(){
             return  stations.getHead();
     }
 
@@ -66,7 +66,7 @@ public:
         if (stations.size() > 0) {
             cout << "Stations IDs: ";
             for (int i = 0; i < stations.size(); i++) {
-                cout << stations[i]->getId();
+                cout << stations[i].getId();
                 if (i < stations.size() - 1)
                     cout << ", ";
             }
@@ -79,11 +79,11 @@ public:
         ostringstream oss;
         oss << id << ",,," << numberOfVehicles << ",,," << price << ",,," << static_cast<int>(vehicleType) << ",,," << name;
         for (int i = 0; i < stations.size(); i++)
-            oss << ",,," << stations[i]->getId();
+            oss << ",,," << stations[i].getId();
         return oss.str();
     }
 
-    static clsTransportLine parse(string line, OpenHash<int, clsStation*>& stations) {
+    static clsTransportLine parse(string line, OpenHash<int, clsStation>& stations) {
         DoubleLinkedList<string> tokens = Split(line,",,,");
         if (tokens.size() < 5) {
             throw invalid_argument("Not enough tokens in line");
@@ -95,10 +95,10 @@ public:
         enVehicleType type = static_cast<enVehicleType>(stoi(tokens[3]));
         string name= tokens[4];
         
-        DoubleLinkedList<clsStation*> stationList;
+        DoubleLinkedList<clsStation> stationList;
         for (int i = 5; i < tokens.size(); i++) {
             int stationId = stoi(tokens[i]);
-            clsStation** station = stations.find(stationId);
+            clsStation* station = stations.find(stationId);
             if (station != nullptr) {
                 stationList.addLast(*station);
             }
@@ -111,4 +111,4 @@ public:
 
 int clsTransportLine::numberOfAllTransportLines = 0;
 
-#endif // TRANSPORTLINE_H
+#endif
