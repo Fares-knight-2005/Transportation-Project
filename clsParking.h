dd16aaea4edb67ec1043d81aa1bc1735591fa8a5
mohmmad-alshiekh <veb2018s@gmail.com>
2025-06-15 14:37:04 +0300

    Update clsParking.h

--- clsParking.h
+++ clsParking.h
@@ -30,13 +30,20 @@ class clsParking{
        return id;    
     }
 
+    void addPassengerTrip(clsPassengerTrip trip) {
+        passengers.enqueue(trip);
+    }
+
     string toString() {
     ostringstream oss;
     oss<< id<< ",,,"<< stationId<< ",,," << distanceToNext << ",,,"  << static_cast<int>(parkingType)<< ",,," << idTransportLine;
+    Queue<clsPassengerTrip> tempPassengers = passengers;
+    while (!tempPassengers.isEmpty()) 
+          oss << ",,," << tempPassengers.dequeue().toString();
     return oss;
     }
 
-    static clsParking parse(string& line) {
+    static clsParking parse(string line) {
     DoubleLinkedList<string> tokens = Split(line,",,,");
          
     if (tokens.size() < 5) {
@@ -48,7 +55,21 @@ class clsParking{
     double distance = stoi(tokens[2]);
     enVehicleType type = static_cast<enVehicleType>(stoi(tokens[3]));
     int idTransportLine= stoi(tokens[4]);
-
+         
+    for (int i = 5; i < tokens.size(); ) {
+            
+            int start = stoi(tokens[i++]);
+            int end = stoi(tokens[i++]);
+            int passengerId = stoi(tokens[i++]);
+            bool heading = (tokens[i++] == "1");
+            bool disabled = (tokens[i++] == "1");
+            bool items = (tokens[i++] == "1");
+            float price = stof(tokens[i++]);
+            
+            clsPassengerTrip trip(start, end, passengerId, heading, disabled, items, price);
+            parking.addPassengerTrip(trip);
+    }
+         
     return clsParking(id, distance, stationId, type, idTransportLine);
     }
 
